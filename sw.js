53f75866d4f8e1048e1ffeaeef019a9dc38171ac
mbsimo95 <bouirigmohamed11@gmail.com>
2026-09-21 22:10:39 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,6 @@
+const CACHE="location-v1";
+const FILES=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png","./icon-180.png"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting();});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim();});
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;
+ e.respondWith(fetch(e.request).then(r=>{const cp=r.clone();caches.open(CACHE).then(c=>c.put(e.request,cp)).catch(()=>{});return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))));});
